54e7dcaa69cc3c880faa02e5a23f612809af2b56
Lars Schöbitz <lars@lse.de>
2021-10-19 13:55:46 +0200

    Prepare data cleaning pipeline

--- R/01-clean-raw-data.R
+++ R/01-clean-raw-data.R
@@ -16,6 +16,7 @@ library(readr)
 library(fs)
 library(stringr)
 library(here)
+library(purrr)
 
 # import data ----------------------------------------------------------
 
@@ -33,3 +34,26 @@ raw_data <- fs::dir_ls("data/raw")
 
 raw_data_xls <- str_subset(raw_data, pattern = ".xls$")
 
+raw_data_xls_paths <- path(here::here(raw_data_xls))
+
+raw_data_list <- list()
+
+for (i in seq_along(raw_data_xls)) {
+  raw_data_list[[i]] <- read_excel(path = here::here(raw_data_xls)[[i]], 
+                                   col_names = c("pm2.5", "pm10", "date"), 
+                                   range = "A1:C10000") %>% 
+    mutate(path = raw_data_xls[[i]]) %>% 
+    mutate(pm2.5 = as.double(pm2.5)) %>% 
+    mutate(pm10 = as.double(pm10)) 
+} 
+
+dat_clean <- bind_rows(raw_data_list) %>% 
+  drop_na() %>% 
+  # reprex help: https://stackoverflow.com/a/35547485/6816220
+  mutate(location = str_extract(string = path, pattern = "(?!.*/).+")) %>% 
+  separate(col = location, into = c("id", "location")) %>% 
+  pivot_longer(cols = pm2.5:pm10, names_to = "indicator", values_to = "value") %>% 
+  mutate(unit = "uq_m3") %>% 
+  select(-path) 
+  
+write_csv(x = dat_clean, file = "data/intermediate/malawi-hospitals-air-quality")
