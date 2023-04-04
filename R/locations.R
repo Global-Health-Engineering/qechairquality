a02317fb33c209d34523409f220a7ea56106cff2
Lars Schöbitz <lars@lse.de>
2023-04-04 15:22:06 +0200

    document locations data

---
+++ R/locations.R
@@ -0,0 +1,13 @@
+#' Sensor locations at Queen Elizabeth Central Hospital
+#' (QECH) in Blantyre, Malawi.
+#'
+#' @format A tibble with 9 rows and 4 variables:
+#' \describe{
+#'   \item{location}{Descriptive name of the location.}
+#'   \item{long}{Longitude coordinate.}
+#'   \item{lati}{Latitude coordinate.}
+#'   \item{type}{Type of location.}
+#' }
+#'
+"locations"
+#
